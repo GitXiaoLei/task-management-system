9b66a58ec64d31549a7fbba17a7127c863b2a8a5
GitXiaoLei <304192604@qq.com>
2017-09-25 12:48:25 +0800

    Mod：修改了config/index.js配置文件，使用其中的proxyTable，能够post跨域请求

--- public/admin/src/api.js
+++ public/admin/src/api.js
@@ -1,14 +1,18 @@
 import axios from 'axios'
-// import qs from 'qs'
+import qs from 'qs'
 // 定义环境：为开发环境时，才设置baseURL和允许发送cookie
 const ENV = 'dev'
 if (ENV === 'dev') {
   axios.defaults.baseURL = 'http://localhost:3000'
-  axios.defaults.withCredentials = true
+  axios.defaults.withCredentials = true // 能够发送、接受cookie
 }
+// 获取权限
+export const getAccessList = params => axios.get('http://localhost:3000/api/access/list', params)
+// 添加权限
+export const addAccess = params => axios.get('/api/access/add', qs.stringify(params))
+// 更新权限
+export const updateAccess = params => axios.get('/api/access/update', qs.stringify(params))
+// 删除权限
+export const delAccess = params => axios.get('/api/access/del', qs.stringify(params))
 
-export const getAccessList = params => axios.get('/admin/access/list', params)
-
-export const login = params => axios.post('/login', {
-  params: params
-})
+export const login = params => axios.post('http://localhost:3000/login', qs.stringify(params))
